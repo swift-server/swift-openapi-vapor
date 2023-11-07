638c1750c75151e30e35b57599bdc58fc96bfd72
MahdiBM <github@mahdibm.com>
2023-11-08 00:42:30 +0330

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-openapi-runtime.git", .upToNextMinor(from: "0.3.0")),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.86.2"),
+        .package(url: "https://github.com/vapor/vapor.git", branch: "async-streaming-body-client-disconnect"),
         .package(url: "https://github.com/apple/swift-atomics", from: "1.2.0")
     ],
     targets: [
